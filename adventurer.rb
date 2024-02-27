0367ab04b359584049869fb6aa3a57233494fdb0
Jeremy Baxter <37813767+jt-is-coding@users.noreply.github.com>
2024-02-27 17:44:01 +0000

    added beginning, working on it now. gotta break.

--- adventurer.rb
+++ adventurer.rb
@@ -20,8 +20,14 @@ class Adventurer
     choice = gets.downcase.chomp
       if choice == "1" || choice == "one" || choice == "create a new adventurer"
         create_adventurer
+        #break
       elsif choice == "2" || choice == "two" || choice == "load my adventurer"
-        #run load_adventurer
+        if self.name == nil
+          puts "There's no Adventurer to load, buddy."
+        else
+          puts "Welcome back, #{self.name} the #{self.job}!"
+        end
+        #break
       elsif choice == "3" || choice == "three" || choice == "exit"
         puts "Sure. See ya later!"
         break
@@ -36,25 +42,38 @@ class Adventurer
     puts "What are we callin this Adventurer?"
     self.name = gets.chomp
     puts "#{self.name}, got it."
-    puts "Here are some available jobs for #{self.name}."
-    puts "1. Busy Bee Keeper"
-    puts "2. Polly-Picks-Your-Pocket"
-    puts "3. Sludgemancer"
-    job_choice = gets.downcase.chomp
     loop {
-    if job_choice == "1" || job_choice == "one" || job_choice == "busy bee keeper"
-      self.job = job_choice
-      break
-    elsif job_choice == "2" || job_choice == "two" || job_choice == "polly-picks-your-pocket"
-      self.job = job_choice
-      break
-    elsifif job_choice == "3" || job_choice == "three" || job_choice == "sludgemancer"
-      self.job = job_choice
-      break
-    else
-      puts "Sorry, that job is taken."
-    end
+      puts "Here are some available jobs for #{self.name}."
+      puts "1. Slacker Knight"
+      puts "2. Polly-Picks-Your-Pocket"
+      puts "3. Sludgemancer"
+      job_choice = gets.downcase.chomp
+      if job_choice == "1" || job_choice == "one" || job_choice == "slacker knight"
+        self.job = "Slacker Knight"
+        break
+      elsif job_choice == "2" || job_choice == "two" || job_choice == "polly-picks-your-pocket"
+        self.job = "Polly-Picks-Your-Pocket"
+        break
+      elsif job_choice == "3" || job_choice == "three" || job_choice == "sludgemancer"
+        self.job = "Sludgemancer"
+        break
+      else
+        puts "Sorry, that job is taken."
+      end
     }
   end
+
+  def beginning
+    if self.job == "Slacker Knight"
+      puts "\n"
+      puts "You are #{self.name} the #{self.job}."
+      #puts "Queen "
+      puts ""
+      puts "..."
+      puts "Or else."
+      puts "Where will you begin your search?"
+      puts "1. The castle bar where tired knights "
+      puts "2. The Queen's living chambers"
+      puts "3. "
 end
   
